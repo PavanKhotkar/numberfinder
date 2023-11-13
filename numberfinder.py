a87add165027bb1640cbc5b25d42b10886b7a185
Pavan <114818017+forzenfly@users.noreply.github.com>
2023-11-13 00:08:40 -0800

    Create numberfinder.py

---
+++ numberfinder.py
@@ -0,0 +1,12 @@
+import phonenumbers
+from phonenumbers import geocoder
+
+# Replace 'your_phone_number' with the actual phone number you want to parse
+number = "+919130867885"  # Example phone number with the country code for India
+
+# Specify the default country region (e.g., "IN" for India)
+default_region = "IN"
+
+parsed_number = phonenumbers.parse(number, default_region)
+location = geocoder.description_for_number(parsed_number, "en")
+print(location)
